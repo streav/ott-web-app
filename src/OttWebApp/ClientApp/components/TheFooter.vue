ed163243832014cc661c64f6a0fc7217a0d2504e
Mohamed Eddami <eddamimo@gmail.com>
2024-02-28 18:41:01 +0100

    refactor footer

--- src/OttWebApp/ClientApp/components/TheFooter.vue
+++ src/OttWebApp/ClientApp/components/TheFooter.vue
@@ -1,12 +1,14 @@
 <template>
   <footer p="y15 x15" flex="~ col gap4">
-    <div flex="~ row gap2" items-center>
+    <div flex="~ row gap3" items-center>
       <div op50 text-sm>
         Made with
       </div>
-      <a href="https://nuxt.com/" target="_blank" title="Nuxt 3">
-        <IconNuxt3 h5 />
+      <a href="https://streav.com/" target="_blank" title="Streav">
+        <IconStreav w-30px h-30px/>
       </a>
     </div>
   </footer>
 </template>
+<script setup lang="ts">
+</script>
