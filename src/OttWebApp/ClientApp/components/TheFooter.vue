0e923c0b7c5978e9ea450154c225f67600fee64f
Mohamed Eddami <eddamimo@gmail.com>
2024-02-29 14:42:18 +0100

    refactor footer

--- src/OttWebApp/ClientApp/components/TheFooter.vue
+++ src/OttWebApp/ClientApp/components/TheFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <footer p="y15 x15" flex="~ col gap4">
-    <div flex="~ row gap3" items-center>
+    <div flex="~ row gap2" items-center>
       <div op50 text-sm>
-        Made with
+        Powered by
       </div>
       <a href="https://streav.com/" target="_blank" title="Streav">
         <IconStreav w-30px h-30px/>
