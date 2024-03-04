92c585830619e8842041087b8e468ff15c9563a7
Mohamed Eddami <eddamimo@gmail.com>
2024-03-04 16:43:32 +0100

    refactor navbar

--- src/OttWebApp/ClientApp/components/NavBar.vue
+++ src/OttWebApp/ClientApp/components/NavBar.vue
@@ -1,36 +1,49 @@
 <template>
   <div
-    flex="~ row lg:col"
-    justify-evenly items-center
-    py5 lg:px5
-    border="t lg:r base"
-    bg-black
+      flex="~ row lg:col"
+      justify-evenly items-center
+      py5 lg:px5
+      border="t lg:r base"
+      bg-black
   >
-    <NuxtLink v-slot="{ isActive }" to="/" title="Home">
-      <div
-        text-2xl
-        :class="isActive ? 'i-ph-house-fill text-primary' : 'i-ph-house'"
-      />
-    </NuxtLink>
-    <NuxtLink v-slot="{ isActive }" to="/movie" title="Movies">
-      <div
-        text-2xl
-        :class="isActive ? 'i-ph-film-strip-fill text-primary' : 'i-ph-film-strip'"
-      />
-    </NuxtLink>
-    <NuxtLink v-slot="{ isActive }" to="/show" title="Shows">
-      <div
-        text-2xl
-        :class="isActive ? 'i-ph-video-fill text-primary' : 'i-ph-video'"
-      />
-    </NuxtLink>
-    <NuxtLink v-slot="{ isActive }" to="/live" title="Live">
-      <div
-          text-2xl
-          :class="isActive ? 'i-ph-television-simple-fill text-primary' : 'i-ph-television-simple'"
-      />
+    <NuxtLink v-for="item in items" v-slot="{ isActive }" :to="item.to" :title="item.title">
+      <div flex flex-col items-center>
+        <div
+            text-2xl
+            :class="isActive ? item.iconActive: item.icon"
+        />
+        <div font-bold :class="isActive ? 'text-primary': ''">
+          {{ item.title }}
+        </div>
+      </div>
     </NuxtLink>
   </div>
 </template>
 <script setup lang="ts">
+const items = [
+  {
+    icon: 'i-ph-house',
+    iconActive: 'i-ph-house-fill text-primary',
+    title: 'Home',
+    to: '/'
+  },
+  {
+    icon: 'i-ph-film-strip',
+    iconActive: 'i-ph-film-strip-fill text-primary',
+    title: 'Movies',
+    to: '/movie'
+  },
+  {
+    icon: 'i-ph-video',
+    iconActive: 'i-ph-video-fill text-primary text-primary',
+    title: 'Shows',
+    to: '/show'
+  },
+  {
+    icon: 'i-ph-television-simple',
+    iconActive: 'i-ph-television-simple-fill text-primary',
+    title: 'Live',
+    to: '/live'
+  }
+]
 </script>
